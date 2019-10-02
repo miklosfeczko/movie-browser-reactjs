165bfe74536be787ca534c8321697fd162e5ccd7
Miklos Feczko <miklosfeczko01@gmail.com>
2019-10-02 02:27:20 +0200

    Added sorting functions for genres.

--- src/components/Genres/Genres.js
+++ src/components/Genres/Genres.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 let count = 1;
+let sort = 'popularity.desc';
 
 class Genres extends Component {
     state = {
@@ -9,7 +10,7 @@ class Genres extends Component {
     }
     
     componentDidMount = async () => {
-        const MOVIE_RESULTS = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${this.props.match.params.name}`);
+        const MOVIE_RESULTS = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=1&with_genres=${this.props.match.params.name}`);
         const DATA = await MOVIE_RESULTS.json();
         this.setState({ 
             MOVIES: DATA.results,
@@ -18,9 +19,11 @@ class Genres extends Component {
     }
 
     fetchMovies() {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${this.props.match.params.name}`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=1&with_genres=${this.props.match.params.name}`)
         .then(response => response.json())
-        .then(DATA => this.setState({ MOVIES: DATA.results }))
+        .then(DATA => this.setState({ 
+                            MOVIES: DATA.results
+                         }))
     }
 
    componentDidUpdate(prevProps) {
@@ -35,7 +38,7 @@ class Genres extends Component {
             MOVIES: []
          })
         count = count+1;
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
         .then(response => response.json())
         .then(DATA => this.setState({ MOVIES: DATA.results }))
         } else return
@@ -45,12 +48,45 @@ class Genres extends Component {
         if (count > 1) {
         this.setState({ MOVIES: []})
         count = count-1;
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
         .then(response => response.json())
         .then(DATA => this.setState({ MOVIES: DATA.results }))
         } else return
     }
- 
+
+    sortChangeTitleAsc = async () => {
+        this.setState({ MOVIES: []})
+        sort = 'original_title.asc'
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
+        .then(response => response.json())
+        .then(DATA => this.setState({ MOVIES: DATA.results }))
+    }
+
+    sortChangePopular = async () => {
+        this.setState({ MOVIES: []})
+        sort = 'popularity.desc'
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
+        .then(response => response.json())
+        .then(DATA => this.setState({ MOVIES: DATA.results }))
+    }
+
+    sortChangeVoteAvg = async () => {
+        this.setState({ MOVIES: []})
+        sort = 'vote_average.desc'
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
+        .then(response => response.json())
+        .then(DATA => this.setState({ MOVIES: DATA.results }))
+    }
+
+    sortChangeReleaseDate = async () => {
+        this.setState({ MOVIES: []})
+        sort = 'release_date.desc'
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=e8146f65b965e0a1cb0600c774f8a2a6&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${count}&with_genres=${this.props.match.params.name}`)
+        .then(response => response.json())
+        .then(DATA => this.setState({ MOVIES: DATA.results }))
+    }
+
+    
     render() {
         return (
             <div className="main__container">
@@ -64,6 +100,10 @@ class Genres extends Component {
                 })}
                 <button onClick={this.backPage}>Back</button>
                 <button onClick={this.nextPage}>Next</button>
+                <button onClick={this.sortChangeTitleAsc}>Sort Title Asc</button>
+                <button onClick={this.sortChangePopular}>Sort Popular</button>
+                <button onClick={this.sortChangeVoteAvg}>Sort Vote Avg</button>
+                <button onClick={this.sortChangeReleaseDate}>Sort Release Date</button>
             </div>
         )
     }
